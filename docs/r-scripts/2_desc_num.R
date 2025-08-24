745044c8862282873ef71872f4b3d138a84de702
fjbeltrantapia <francisco.beltran.tapia@ntnu.no>
2025-08-24 17:13:57 +0200

    Updating other sources

--- docs/r-scripts/2_desc_num.R
+++ docs/r-scripts/2_desc_num.R
@@ -20,39 +20,47 @@ data <- read_rds("data/paisley_v2.rds")
 data
 
 # Reporting frequencies is not useful when there are many values
-data %>%  
-  count(age) %>%              
-  mutate(perc = 100*n/sum(n)) %>%
+data |>  
+  count(age) |>              
+  mutate(perc = 100*n/sum(n)) |>
   print(n = Inf) # display all rows
 
-paisley_data %>% 
-  count(age) %>%
+paisley_data |> 
+  count(age) |>
   print(n = Inf) # display all rows
 
 # Potential solution: Create class intervals (group values into bins)
-data <- data %>% 
+data <- data |> 
   mutate(age_class = cut(age, breaks = 5)) # Creates groups of equal size
 
-data %>% 
-  count(age_class) %>%
+data |> 
+  count(age_class) |>
   print(n = Inf) # display all rows
 
 # or setting when the breaks happen yourself
-data %>% 
-  mutate(age_class = cut(age, breaks = c(0, 14, 19, 50, Inf))) %>%
-  count(age_class) %>%
+data |> 
+  mutate(age_class = cut(age, breaks = c(0, 14, 19, 50, Inf))) |>
+  count(age_class) |>
   mutate(perc = 100*n/sum(n)) # report relative frequencies as well
 
+# display the data visually with ggplot() and geom_col()
+data |>
+  mutate(age_class = cut(age, 
+                         breaks = c(0, 14, 19, 50, Inf))) |>
+  count(age_class) |>
+  ggplot(aes(x = age_class, y = n)) +
+  geom_col()
+                         
 # or even assign "labels" to those groups
-data %>% 
+data |> 
   mutate(age_class = cut(age, breaks = c(0, 14, 19, 50, Inf), 
-                         labels = c("Children", "Youngters", "Adults", "Elderly"))) %>%
+                         labels = c("Children", "Youngters", "Adults", "Elderly"))) |>
   count(age_class)
   
 
 
 ## Histogram (visual representation of the distribution)
-data %>%   
+data |>   
   ggplot(mapping = aes(x = age)) +
     geom_histogram(binwidth = 5)
 
@@ -60,11 +68,11 @@ data %>%
   # ggplot uses the + operator (to add layers)
   # 3 key aspects: data, aesthetics and type
 
-g1 <- data %>%   
+g1 <- data |>   
   ggplot(aes(x = age)) +
     geom_histogram(binwidth = 1)
 
-g2 <- data %>%   
+g2 <- data |>   
   ggplot(aes(x = age)) +
     geom_histogram(binwidth = 10)
 
@@ -75,13 +83,13 @@ g1 + g2
 ggsave("output/hist_age.pdf") # save plot
 
 # Focusing on particular subsamples
-data %>%   
-  filter(sex == "male") %>% 
+data |>   
+  filter(sex == "male") |> 
   ggplot() +
   geom_histogram(mapping = aes(x = age), binwidth = 1)
 
 # Or by group
-data %>%   
+data |>   
   ggplot() +
   geom_histogram(mapping = aes(x = age), binwidth = 5) +
   facet_wrap(~ sex, nrow = 1)
@@ -93,7 +101,7 @@ data %>%
 # !is.na(x), n_distinct(x)
 
 # average age
-data %>% 
+data |> 
   summarize(mean_age = mean(age, na.rm = TRUE))
   # na.rm removes missing values from the computations
   # stands for "NA remove"
@@ -102,7 +110,7 @@ data %>%
 
 # first assign a name and then define what you want to do using functions
 
-data %>% 
+data |> 
   summarize(
     count = sum(!is.na(age)),   # "!is.na" indicates "is not na" (not available=missing value)
     mean_age = mean(age, na.rm = TRUE), # the comma allows for asking for more statistics
@@ -114,7 +122,7 @@ data %>%
 
 
 # Illustration:
-data %>%   
+data |>   
   ggplot(mapping = aes(x = age)) +
     geom_histogram(colour = "grey70", alpha = 0.2) +
     geom_vline(aes(xintercept=29.6)) +
@@ -130,7 +138,7 @@ data %>%
     annotate(x=89, label="Max", y=150, colour="red", geom = "label", size = 3) +
     annotate(x=17, label="p10", y=150, colour="red", geom = "label", size = 3) +  
     annotate(x=47, label="p90", y=150, colour="red", geom = "label", size = 3) +
-    annotate(x=29.6, label="Standard deviation", y=5, colour="red", geom = "label", size = 3) +
+    annotate(x=29.6, label="+1/-1 Standard deviation", y=5, colour="red", geom = "label", size = 3) +
     scale_x_continuous(name = "Age", breaks = seq(0,90,10)) +
     scale_y_continuous(name = "Frequency")
 
@@ -138,61 +146,29 @@ data %>%
 # Shape of the distribution: differences
 
 library(patchwork)
-p1 <- data %>%  
-  filter(age>15) %>%
+p1 <- data |>  
+  filter(age>15) |>
   ggplot(mapping = aes(x = weight)) +
   geom_histogram()
 
-p2 <- data %>%
-  filter(age>15) %>%
+p2 <- data |>
+  filter(age>15) |>
   ggplot(mapping = aes(x = age)) +
   geom_histogram()
 
 p1 + p2
 
-# Summarise by group(s): 
-data %>% 
-  group_by(countryb) %>%
-  summarize(
-    count = sum(!is.na(age)),
-    mean_age = mean(age, na.rm = TRUE),
-    min_age = min(age, na.rm = TRUE),
-    max_age = max(age, na.rm = TRUE)
-  )
-
-# by several groups
-paisley %>% 
-  group_by(sex, countryb) %>%
-  summarize(
-    count = sum(!is.na(age)), 
-    mean_age = mean(age, na.rm = TRUE),
-    median_age = median(age, na.rm = TRUE)
-  )
-
-# store the results in a new object
-by_sex_countryb <- paisley_data %>%   
-  group_by(sex, countryb) %>%
-  summarize(count = sum(!is.na(age)), 
-            mean_age = mean(age, na.rm = TRUE),
-            median_age = median(age, na.rm = TRUE)
-  )
-
-view(by_sex_countryb)
-
-# Export to excel (note that is appended as a differet sheet in the previous file)
-library("writexl")
-writexl(by_sex_countryb, path = "table2.xlsx")
 
 
 ### Modifying and creating numerical variables
 
 # Inspect info on heights: feet, inches
 
-g1 <- data %>%   
+g1 <- data |>   
   ggplot(aes(x = feet)) +
   geom_histogram()
 
-g2 <- data %>%   
+g2 <- data |>   
   ggplot(aes(x = inches)) +
   geom_histogram()
 
@@ -200,51 +176,44 @@ g2 <- data %>%
 library(patchwork)
 g1 + g2 # put the graphs together
 
-data %>%   
-  ggplot(aes(x = age)) +
-  geom_histogram()
-
-
 # something is going on
 
-data %>% 
-  subset(age>100) # Identify this case
+data |> 
+  subset(feet>20) # Identify this case
 
 ## Correct numerical value (typo)
-data <- data %>%
-  mutate(age = replace(age, age == 160, 16))
+data <- data |>
+  mutate(feet = replace(feet, case==99 & feet==50, 5))
 
 
 # check you dit it right
-data %>% 
+data |> 
   subset(forename=="FRANCIS" & surname=="GILFILLAN", 
-         select=c(forename, surname, age))
-
-
+         select=c(casen, forename, surname, feet))
 
 
 # Create nex variable (height) combining both variables (feet & inches)
-data <- data %>%
+data <- data |>
   mutate(height = 30.48*feet+2.54*inches)
 
 view(data)
 # a new variable has been added (last column on the right)
 
 # visualise new variable
-data %>%  
+data |>  
   ggplot() +
   geom_histogram(mapping = aes(x = height), binwidth = 5)
 
 
 # Group a numerical variable into different bins
-data <- data %>%
+data <- data |>
   mutate(height_bins = case_when(height < 145 ~ 'low',
                                  height >=145 & height< 175 ~ 'med',
                                  height >=175 ~ 'high'))
 # check
-data %>% 
-  group_by(height_bins) %>%
-  summarize(n = n()) %>%
+data |> 
+  group_by(height_bins) |>
+  summarize(n = n()) |>
   mutate(freq = n/sum(n))
 
 
@@ -252,39 +221,48 @@ data %>%
 
 # Allow quantifying qualitative variables
 
-data %>%
+data |>
   count(literacy)
 
-data <- data %>%
-  mutate(lit_adj = recode(literacy, 
-                          "superior education" = "write", 
-                          "read well" = "read", 
-                          "read tolerably" = "read", 
-                          "read a little" = "read",
-                          "read & write well" = "write", 
-                          "read & write tolerably" = "write", 
-                          "read & write a little" = "read", 
-                          "cannot write" = "read")
-  )
+  # example with case_when()
+data <- data |>
+  mutate(write = case_when(
+    literacy=="superior education" ~ 1,
+    literacy=="read & write well" ~ 1,
+    literacy=="read & write tolerably" ~ 1,
+    is.na(literacy) ~ NA,
+    TRUE ~ 0))
 
-data %>%
+data |>
+  count(write)
+
+data |>
+  summarize(
+    obs = sum(!is.na(write)),
+    mean = mean(write, na.rm = TRUE),
+    sd = sd(write, na.rm = TRUE),
+    min = min(write, na.rm = TRUE),
+    max = max(write, na.rm = TRUE)) |>
+  mutate_if(is.numeric, round, 3)
+
+  # examples with if_else
+data |>
   count(countryb)
 
+data <- data |>
+  mutate(scotland = if_else(countryb=="scotland", 1, 0)) 
 
-data <- data %>%
-  mutate(scotland = ifelse(countryb=="scotland", 1, 0)) 
-
-data %>%
+data |>
   select(countryb, scotland) 
 
 # summary statistics
-data %>% 
+data |> 
   summarize(
     obs = sum(!is.na(scotland)),   
     mean = mean(scotland, na.rm = TRUE), 
     sd = sd(scotland, na.rm = TRUE), 
     min = min(scotland, na.rm = TRUE),
-    max = max(scotland, na.rm = TRUE)) %>%
+    max = max(scotland, na.rm = TRUE)) |>
   mutate_if(is.numeric, round, 3)
 
   # the average of a dummy variable reflects the fraction (proportion) of 
@@ -301,10 +279,10 @@ data %>%
 
 ### More on graphing with ggplot
 # 3 key aspects: data, aesthetics (mapping) and type
-paisley_data %>%  
-  filter(age >= 18) %>%
-  group_by(countryb) %>%
-  summarize(mean_height = mean(height, na.rm = TRUE)) %>%
+paisley_data |>  
+  filter(age >= 18) |>
+  group_by(countryb) |>
+  summarize(mean_height = mean(height, na.rm = TRUE)) |>
   ggplot(mapping = aes(x = countryb, y = mean_height)) +
   geom_bar()
 # you can run chunks of code to see what the do so far
--- r-scripts/1_desc_qual.R
+++ r-scripts/1_desc_qual.R
@@ -29,9 +29,6 @@ setwd("/Volumes/francijb/Documents/FRAN/Teaching/QM_2024/session")
 
 ### Install packages (if needed; only once)
 # install.packages("tidyverse")
-# install.packages("knitr")
-# install.packages("xlsx")
-# install.packages("writexl")
 
     # R works using different tools that are contained
     # in different packages
@@ -40,11 +37,10 @@ setwd("/Volumes/francijb/Documents/FRAN/Teaching/QM_2024/session")
 
 ### Load the packages you need (you will need to do it every session)
 library(tidyverse)
-library(knitr)
 library(readxl)
 
 # Import data
-data <- read_excel("data/paisley_data.xls")
+data <- read_excel("data/paisley-data.xls")
   # notice that we are using a "relative path"  
   # if the data is in the working directory, you don't need the path (just the file name)
   # if the data is somewhere else (not within the project environment), you need to use the absolute path
@@ -71,10 +67,10 @@ tail(data) # the last observations
 
 tbl_vars(data) # list the variables (fields) in the dataset
 
-## We will be using the pipe (%>%) though
+## We will be using the pipe (|> or |>) though
 
-data %>%  
-  head(15)
+data |>  
+  print(15)
 
 ## NO NEED TO LEARN THE COMMANDS BY HEART
   # copy and paste from the templates
@@ -97,66 +93,66 @@ data %>%
 
   # number of males and females
 
-data %>%  # returns a table
+data |>  # returns a table
   count(sex)
   # the pipe (also |>) meaning "and then"
   # it takes the output of a line of code and uses it as input to the next line
 
-data %>%  # returns a table
+data |>  # returns a table
   count(employed)
   # notice that is also reports the number of missing values
 
-data %>%  # returns a table
-  count(occup) %>%
+data |>  # returns a table
+  count(occup) |>
   print(n = 20) # display 20 rows
 
-data %>%  # returns a table
-  count(occup) %>%
+data |>  # returns a table
+  count(occup) |>
   print(n = Inf) # display all rows
 
-data %>%  
+data |>  
   count(occup, sort = TRUE) # present the data in order
                             # important to quickly identify the most important categories
 
 ## Absolute and relative frequencies
-data %>% 
-  count(occup, sort = TRUE) %>%              
+data |> 
+  count(occup, sort = TRUE) |>              
   mutate(proportion = n/sum(n))  # adding relative frequency 
 
-data %>% 
-  count(occup, sort = TRUE) %>%              
-  mutate(prop = n/sum(n)) %>% # fractions
+data |> 
+  count(occup, sort = TRUE) |>              
+  mutate(prop = n/sum(n)) |> # fractions
   mutate(perc = 100*n/sum(n)) # percentages 
 
 # or
-data %>% 
-  group_by(sex) %>%       # dimension we focus on
-  summarize(n = n()) %>%  # reporting number of observations in each group
+data |> 
+  group_by(sex) |>       # dimension we focus on
+  summarize(n = n()) |>  # reporting number of observations in each group
   mutate(rel_freq = n/sum(n)) # creating a new variable with the relative frequency 
 
   # notice that the code may look tricky but is easily replicable
   # with literacy
-data %>% 
-  group_by(literacy) %>%
-  summarize(n = n()) %>%
+data |> 
+  group_by(literacy) |>
+  summarize(n = n()) |>
   mutate(freq = n/sum(n))
 
 # Adding another row with the totals:
 library(janitor)
 
-data %>%  
-  count(countryb, sort = TRUE) %>%              
-  mutate(perc = 100*n/sum(n)) %>%
-  adorn_totals("row") %>%
+data |>  
+  count(countryb, sort = TRUE) |>              
+  mutate(perc = 100*n/sum(n)) |>
+  adorn_totals("row") |>
   as_tibble()
 
 
 
 ### Focusing on subsamples (smaller groups) 
   # filter()
-data %>% 
-  filter(sex == "male") %>%   
-  count(literacy, sort = TRUE) %>%
+data |> 
+  filter(sex == "male") |>   
+  count(literacy, sort = TRUE) |>
   mutate(prop = n/sum(n))
   
   # "filter" restricts the analysis to those observations fulfilling that condition
@@ -167,23 +163,23 @@ data %>%
       # or: |
       # except: - 
 
-data %>% 
-  filter(sex=="male" & age>=18 & age<=50) %>%   
-  count(literacy, sort = TRUE) %>%              
+data |> 
+  filter(sex=="male" & age>=18 & age<=50) |>   
+  count(literacy, sort = TRUE) |>              
   mutate(prop = n/sum(n)) 
 
-data %>% 
-  filter(countryb=="ireland" | countryb=="scotland") %>%
-  filter(sex=="male" & age>=18 & age<=50) %>%   
-  count(literacy, sort = TRUE) %>%              
-  mutate(prop = n/sum(n)) %>%
+data |> 
+  filter(countryb=="ireland" | countryb=="scotland") |>
+  filter(sex=="male" & age>=18 & age<=50) |>   
+  count(literacy, sort = TRUE) |>              
+  mutate(prop = n/sum(n)) |>
   mutate(prop = round(prop, 2)) # reporting only up two decimals
 
 # This allows paying attention to what it is both common and rare
-data %>% 
-  filter(countryb=="ireland" | countryb=="scotland") %>%
-  filter(sex=="male" & age>=18 & age<=50) %>%   
-  filter(literacy=="superior education") %>%
+data |> 
+  filter(countryb=="ireland" | countryb=="scotland") |>
+  filter(sex=="male" & age>=18 & age<=50) |>   
+  filter(literacy=="superior education") |>
   select(year, forename, surname, age, born, countryb, reside, occup)
 
 
@@ -191,13 +187,14 @@ data %>%
   # the output is not saved unless it is assigned to an object
   # which can then be exported (saved) into different formats later
 
-table1 <- data %>% 
-  filter(sex == "male") %>%   
-  group_by(literacy) %>%      
-  summarize(n = n()) %>%
+table1 <- data |> 
+  filter(sex == "male") |>   
+  group_by(literacy) |>      
+  summarize(n = n()) |>
   mutate(freq = n/sum(n)) 
 
 # Export the output (object) into excel 
+install.packages("writexl")
 library(writexl)
 
 write_xlsx(occ, "output/table1.xlsx")
@@ -206,7 +203,7 @@ write_xlsx(occ, "output/table1.xlsx")
 
 
 ###### Plotting frequencies (graph bar)
-  # ggplot
+  # ggplot (part of the tidyverse)
   # use "+" to add features to the graph
 
 # sex
@@ -215,34 +212,34 @@ ggplot(data = data) +
     # "aes" goes for aesthetics
 
   # or
-data %>% 
+data |> 
   ggplot() + 
     geom_bar(aes(x = sex))
 
 # literacy
-data %>% 
+data |> 
   ggplot() + 
     geom_bar(aes(x = literacy))
 
 # horizontal to facilitate reading the categories
-data %>% 
+data |> 
   ggplot() + 
     geom_bar(aes(x = literacy)) +
     coord_flip()
 
 # narrowing down the analysis  
-data %>% 
-  filter(countryb == "scotland" | countryb == "ireland") %>%
-  filter(sex == "male" & age>18) %>%
+data |> 
+  filter(countryb == "scotland" | countryb == "ireland") |>
+  filter(sex == "male" & age>18) |>
   ggplot() + 
     geom_bar(mapping = aes(x = literacy)) +
     coord_flip()
 
 
 # proportions (instead of counts)
-data %>% 
-  count(literacy, sort = TRUE) %>%              
-  mutate(prop = n/sum(n)) %>%
+data |> 
+  count(literacy, sort = TRUE) |>              
+  mutate(prop = n/sum(n)) |>
   ggplot(aes(x = literacy, y = prop)) + 
     geom_col() + # instead of geom_bar()
     coord_flip()
@@ -251,9 +248,9 @@ data %>%
     # we explicitly indicate what we want to depict (prop in this case)
 
 # editing the graph
-g1 <- data %>% 
-  count(literacy, sort = TRUE) %>%              
-  mutate(prop = n/sum(n)) %>%
+g1 <- data |> 
+  count(literacy, sort = TRUE) |>              
+  mutate(prop = n/sum(n)) |>
   ggplot(mapping = aes(x = literacy, y = prop)) + 
     geom_col() + 
     coord_flip() + 
@@ -265,7 +262,7 @@ g1 <- data %>%
 
 g1  
 # save the graph
-g1 %>%
+g1 |>
   ggsave("output/lit.png", dpi = 320)
 
 ?ggsave
@@ -275,30 +272,29 @@ g1 %>%
 ### Re-categorising qualitative information 
 # typos, aggregating categories, etc.
 
-data %>%  
-  count(occup) %>%              
+data |>  
+  count(occup) |>              
   mutate(prop = n/sum(n)) 
 
 # recoding
-data <- data %>%
-  mutate(occup_adj = recode(occup, 
+data <- data |>
+  mutate(occup_adj = recode(occup,
                             "blacksmith" = "black smith", 
-                            "block print" = "block printer")
-         )
+                            "block print" = "block printer"))
 
 View(data)
 
-data %>% 
-  filter(occup_adj=="black smith" | occup_adj=="block printer") %>%
+data |> 
+  filter(occup_adj=="black smith" | occup_adj=="block printer") |>
   select(occup, occup_adj)
 
-data %>%  
+data |>  
   count(literacy)
 
   # checking what we have done
 
 # another example with literacy: iliterate, read, write
-data <- data %>%
+data <- data |>
   mutate(lit_adj = recode(literacy, 
                           "superior education" = "write", 
                           "read well" = "read", 
@@ -311,55 +307,56 @@ data <- data %>%
   )
     # no need to recode "illiterate" because it is the same category
 
-data %>% 
+data |> 
   subset(select=c(forename, surname, literacy, lit_adj))
 
-data %>%
+data |>
   count(lit_adj)
 
 # removing leading and trailing spaces
-data <- data %>% 
+data <- data |> 
   mutate(occup_adj = str_trim(occup_adj))
 
-# ranking "qualitative" information
-data %>%  
-  count(literacy) %>%              
+# ranking "qualitative" information (ordinal variables)
+data |>  
+  count(literacy) |>              
   mutate(prop = n/sum(n)) 
   # listed in alphabetical order (not always informative enough)
 
 
 # factor variables: ranking categories
-data <- data %>% 
+data <- data |> 
   mutate(lit_rank = factor(literacy, 
                            levels = c("illiterate", 
                                       "read a little", 
                                       "read tolerably", 
-                                      "read well", "cannot write", 
+                                      "read well", 
+                                      "cannot write", 
                                       "read & write a little", 
                                       "read & write tolerably", 
                                       "read & write well", 
                                       "superior education"), 
                            ordered = TRUE))
 
-data %>%
+data |>
   count(lit_rank)
   
   # or visually:
-data %>% 
-  filter(!is.na(lit_rank)) %>% # exclude missing values
+data |> 
+  filter(!is.na(lit_rank)) |> # exclude missing values
   ggplot() +
     geom_bar(aes(x = lit_rank)) +
     coord_flip()  
 
 # re-categorising more complicate patterns
 
-data %>%
+data |>
   count(marks)
 
 # install.packages("stringr")
 library(stringr)
 
-data <- data %>%
+data <- data |>
   mutate(marks_adj = case_when(
     str_detect(marks, "scar") ~ "scar",
     str_detect(marks, "cut") ~ "cut", 
@@ -372,19 +369,26 @@ data <- data %>%
     TRUE ~ "other"))
 
 # checking what you have done
-data %>%
+data |>
   select(marks, marks_adj)
 
-data %>% 
+data |> 
   count(marks_adj)
 
-data %>% 
-  filter(marks_adj=="cut") %>% 
-  count(marks) %>% 
+data |> 
+  filter(marks_adj=="cut") |> 
+  count(marks) |> 
   print(n = Inf)
 
+# the above matches the exact sequence of characters 
+  # defined within the quotation marks.
 
-# Regular expressions
+# Regular expressions - trick
+  # "^A" those starting with A
+  # "e$" those ending with e
+  # “cut|scar” would match both possibilities
+  # !str_detect(mark,"cut") -- all except those containing “cut”. T
+  # more when explaining "textual data"
 
 
 ##### Save the (modified) data ####
